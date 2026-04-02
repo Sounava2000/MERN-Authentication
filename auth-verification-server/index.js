fe78392dd9438c04462d00da2fec2fb4259cb764
Sounava Mukherjee <soumo2640@gmail.com>
2026-04-02 21:18:39 +0530

    fix path-to-regexp error

--- auth-verification-server/index.js
+++ auth-verification-server/index.js
@@ -14,7 +14,7 @@ app.use(cors({
   credentials: true
 }));
 console.log("NEW VERSION DEPLOYED");
-app.options("*", cors());
+app.options("/*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/v1", userRouter);
