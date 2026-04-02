40f23244e4ef627aabe54d914c51b3db5053030c
Sounava Mukherjee <soumo2640@gmail.com>
2026-04-02 18:14:02 +0530

    update cors

--- auth-verification-server/index.js
+++ auth-verification-server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import userRouter from "./routes/userRoutes.js";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import cors from "cors";
 import { dbConnect } from "./config/dbConnect.js";
 import { ErrorMiddleware } from "./middlewares/error.js";
@@ -10,24 +10,31 @@ dotenv.config();
 const port = process.env.PORT;
 
 const allowedOrigins = [
-  'http://localhost:5173',
-    'http://localhost:5174',
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://mern-authentication-d2kzmgccz-sounavas-projects.vercel.app",
 ];
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/v1",userRouter);
+app.use("/v1", userRouter);
 app.use(ErrorMiddleware);
 dbConnect()
   .then(() =>
     app.listen(port, () => {
       console.log(`Example app listening on  ${port}`);
-    })
+    }),
   )
   .catch((err) => console.log(err.message));
- 
- 
